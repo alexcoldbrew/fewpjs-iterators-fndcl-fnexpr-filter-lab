d2e4d2df337fdb16febdd72ff10cf6febc215895
alexcoldbrew <alexcoldbrew@gmail.com>
2021-03-14 14:28:41 -0500

    commit tree ca7c66647c8aa222b15f611b81d7209fbb368ab8

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+// typical function writing
+
+function findMatching(drivers, string) {
+    return drivers.filter(n => n.toLowerCase() === string.toLowerCase())
+}
+
+// should be writing these functions this way?
+
+const fuzzyMatch = (drivers, string) => {
+    return drivers.filter(n => n.slice(0, string.length) === string)
+}
+
+const matchName = (drivers, string) => {
+    return drivers.filter(n => n.name === string)
+}
